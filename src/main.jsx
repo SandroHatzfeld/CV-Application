c6a12eb528e78cdd8d90a76cde255bb9cb439198
Sandro Hatzfeld <sandrohatzfeld@gmail.com>
2025-03-30 17:31:35 +0200

    sorting and start visualizing the cv

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./styles/index.css"
-import MiscMenu from "./components/MiscMenu.jsx"
-import CVRenderer from "./components/CVRenderer.jsx"
-import Menu from "./components/Menu.jsx"
+import MiscMenu from "./components/menu/MiscMenu.jsx"
+import CVRenderer from "./components/rendering/CVRenderer.jsx"
+import Menu from "./components/menu/Menu.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
