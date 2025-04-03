bc834ede19e3b757a80cdaf51ea711a8ec83f6b9
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-04-03 14:37:12 +0200

    layout changes

--- src/components/rendering/components/CVLayout1.jsx
+++ src/components/rendering/components/CVLayout1.jsx
@@ -2,12 +2,18 @@ import HeaderLayout1 from './layout1/HeaderLayout1.jsx'
 import ListRenderer from './ListRenderer.jsx'
 
 export default function CVLayout1(props) {
+  
+
   return (
     <div id="CVLayout1">
       <HeaderLayout1 {...props}/>
       <main>
-        <ListRenderer items={props.itemsEducation} title="Education" />
-        <ListRenderer items={props.itemsBusiness} title="Experience" />
+        <div>
+          {props.dataPersonal.description && <h2>About Me:</h2>}
+          {props.dataPersonal.description && <p>{props.dataPersonal.description}</p>}
+        </div>
+        <ListRenderer items={props.itemsEducation} title="Education" itemHeading="" itemDescription="Degree"/>
+        <ListRenderer items={props.itemsBusiness} title="Experience" itemHeading="" itemDescription="Position"/>
       </main>
       <footer>
         {props.dataPersonal.street && <h2>Adress:</h2>}
--- src/components/rendering/components/ListRenderer.jsx
+++ src/components/rendering/components/ListRenderer.jsx
@@ -1,21 +1,26 @@
 export default function ListRenderer(props) {
-	if(props.items.length !== 0) {
-		return (
-			<div className="list">
-				<h2>{props.title}</h2>
-				{props.items.map((item) => {
-					return (
-						<div className="list-item" key={item.name}>
-							<div className="list-item-time">
-								{item.start}
-								<br />
-								{item.end}
-							</div>
-							<div className='list-item-details'><h3>{item.name}</h3><p>{item.description}</p></div>
-						</div>
-					)
-				})}
-			</div>
-		)
-	}
+	
+  if (props.items.length !== 0) {
+    return (
+      <div className="list">
+        <h2>{props.title}</h2>
+        {props.items.map((item) => {
+          return (
+            <div className="list-item" key={item.name}>
+              <div className="list-item-time">
+                {new Date(item.start).toLocaleDateString()}
+                <br />
+                {new Date(item.end).toLocaleDateString()}
+              </div>
+              <div className="list-item-details">
+                <h3>{props.itemHeading && props.itemHeading + ": "}{item.name}</h3>
+                {item.description && <p>{props.itemDescription && props.itemDescription + ": "}{item.description}</p>}
+                {item.location && <p>Location: {item.location}</p>}
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
