04e00f9e91dee612c66e029ec5f49d6ef88417a5
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-04-03 16:21:16 +0200

    adding values to layoutoptions

--- src/components/settings/layoutOptions.jsx
+++ src/components/settings/layoutOptions.jsx
@@ -1,62 +1,142 @@
 export const layoutOptions = [
-	{
-		name: "Topheavy",
-		image: "./assets/layouts/topheavy.svg",
-		colors: [
-			{ displayName: "Main Color", value: "#416fef" },
-			{ displayName: "Accent Color", value: "#efab41" },
-			{ displayName: "Text Color", value: "#3b3b3b" },
-			{ displayName: "Background Color", value: "#ffffff" },
-		],
-		fonts: [
-			{ displayName: "Heading" },
-			{ displayName: "Paragraph" },
-			{ displayName: "Footer" },
-		],
-	},
-	{
-		name: "Sidebar",
-		image: "./assets/layouts/sidebar.svg",
-		colors: [
-			{ displayName: "Main Color", value: "#b43838" },
-			{ displayName: "Accents Color", value: "#75c02e" },
-			{ displayName: "Text Color", value: "#3b3b3b" },
-			{ displayName: "Background Color", value: "#ffffff" },
-		],
-		fonts: [
-			{ displayName: "Heading" },
-			{ displayName: "Paragraph" },
-			{ displayName: "Footer" },
-		],
-	},
-	{
-		name: "Floating",
-		image: "./assets/layouts/floating.svg",
-		colors: [
-			{ displayName: "Main Color", value: "#f9f6b6" },
-			{ displayName: "Secondary Color", value: "#2fc07a" },
-			{ displayName: "Text Color", value: "#eeeeee" },
-			{ displayName: "Background Color", value: "#3b3b3b" },
-		],
-		fonts: [
-			{ displayName: "Heading" },
-			{ displayName: "Paragraph" },
-			{ displayName: "Footer" },
-		],
-	},
-	{
-		name: "Boxes",
-		image: "./assets/layouts/boxes.svg",
-		colors: [
-			{ displayName: "Main Color", value: "#375199" },
-			{ displayName: "Box Color", value: "#142d72" },
-			{ displayName: "Text Color", value: "#e1e1e1" },
-			{ displayName: "Background Color", value: "#dddddd" },
-		],
-		fonts: [
-			{ displayName: "Heading" },
-			{ displayName: "Paragraph" },
-			{ displayName: "Footer" },
-		],
-	},
+  {
+    name: "Topheavy",
+    image: "./assets/layouts/topheavy.svg",
+    colors: [
+      {
+        displayName: "Main Color",
+        name: "mainColor",
+        defaultValue: "#416fef",
+        value: "",
+      },
+      {
+        displayName: "Accent Color",
+        name: "accentColor",
+        defaultValue: "#efab41",
+        value: "",
+      },
+      {
+        displayName: "Text Color",
+        name: "textColor",
+        defaultValue: "#3b3b3b",
+        value: "",
+      },
+      {
+        displayName: "Background Color",
+        name: "backgroundColor",
+        defaultValue: "#ffffff",
+        value: "",
+      },
+    ],
+    fonts: [
+      { displayName: "Heading", value: "" },
+      { displayName: "Paragraph", value: "" },
+      { displayName: "Footer", value: "" },
+    ],
+  },
+  {
+    name: "Sidebar",
+    image: "./assets/layouts/sidebar.svg",
+    colors: [
+      {
+        displayName: "Main Color",
+        name: "mainColor",
+        defaultValue: "#b43838",
+        value: "",
+      },
+      {
+        displayName: "Accents Color",
+        name: "accentColor",
+        defaultValue: "#75c02e",
+        value: "",
+      },
+      {
+        displayName: "Text Color",
+        name: "textColor",
+        defaultValue: "#3b3b3b",
+        value: "",
+      },
+      {
+        displayName: "Background Color",
+        name: "backgroundColor",
+        defaultValue: "#ffffff",
+        value: "",
+      },
+    ],
+    fonts: [
+      { displayName: "Heading", value: "" },
+      { displayName: "Paragraph", value: "" },
+      { displayName: "Footer", value: "" },
+    ],
+  },
+  {
+    name: "Floating",
+    image: "./assets/layouts/floating.svg",
+    colors: [
+      {
+        displayName: "Main Color",
+        name: "mainColor",
+        defaultValue: "#f9f6b6",
+        value: "",
+      },
+      {
+        displayName: "Secondary Color",
+        name: "secondaryColor",
+        defaultValue: "#2fc07a",
+        value: "",
+      },
+      {
+        displayName: "Text Color",
+        name: "textColor",
+        defaultValue: "#eeeeee",
+        value: "",
+      },
+      {
+        displayName: "Background Color",
+        name: "backgroundColor",
+        defaultValue: "#3b3b3b",
+        value: "",
+      },
+    ],
+    fonts: [
+      { displayName: "Heading", value: "" },
+      { displayName: "Paragraph", value: "" },
+      { displayName: "Footer", value: "" },
+    ],
+  },
+  {
+    name: "Boxes",
+    image: "./assets/layouts/boxes.svg",
+    colors: [
+      {
+        displayName: "Main Color",
+        name: "mainColor",
+        defaultValue: "#375199",
+        value: "",
+      },
+      {
+        displayName: "Box Color",
+        name: "boxColor",
+        defaultValue: "#142d72",
+        value: "",
+      },
+      {
+        displayName: "Text Color",
+        name: "textColor",
+        defaultValue: "#e1e1e1",
+        value: "",
+      },
+      {
+        displayName: "Background Color",
+        name: "backgroundColor",
+        defaultValue: "#dddddd",
+        value: "",
+      },
+    ],
+    fonts: [
+      { displayName: "Heading", value: "" },
+      { displayName: "Paragraph", value: "" },
+      { displayName: "Footer", value: "" },
+    ],
+  },
 ]
